c5ce803a358ea209248138745555d1795533e36a
Admin <you@example.com>
2020-02-12 12:08:55 +0300

    Updates

---
+++ precache-manifest.8261daef0b84f593e5f8d759804911e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43d6335e7195e0c1e89d74914ef5170",
+    "url": "/test-game-squares/index.html"
+  },
+  {
+    "revision": "b08e6b8bb306ffb2e6f1",
+    "url": "/test-game-squares/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cbb594372b41ab952bdb",
+    "url": "/test-game-squares/static/css/main.8a87a8a8.chunk.css"
+  },
+  {
+    "revision": "b08e6b8bb306ffb2e6f1",
+    "url": "/test-game-squares/static/js/2.bd60935e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/test-game-squares/static/js/2.bd60935e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbb594372b41ab952bdb",
+    "url": "/test-game-squares/static/js/main.32ebaf3b.chunk.js"
+  },
+  {
+    "revision": "b1a5fca32c2485fc791b",
+    "url": "/test-game-squares/static/js/runtime-main.a7bf60d5.js"
+  },
+  {
+    "revision": "500dce705ae690f23a9cc449573845b5",
+    "url": "/test-game-squares/static/media/error.500dce70.png"
+  }
+]);
